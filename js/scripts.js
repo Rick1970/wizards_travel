5f18b10e78e7359de6529313ee988e48efff0e11
Rick Thornbrugh <rthornbrug@gmail.com>
2016-06-17 12:12:21 -0700

    added toggling block2

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,25 @@ $(document).ready(function() {
     $("#wizard-hidden").toggle();
   });
 });
+
+$(document).ready(function(event) {
+  $("form#get-name").submit(function(event) {
+    var name = $("input#name").val();
+    event.preventDefault();
+    });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+  $("#block2-showing").toggle();
+  $("#block2-hidden").toggle();
+
+    event.preventDefault();
+
+  });
+
+
+
+
+
+});
