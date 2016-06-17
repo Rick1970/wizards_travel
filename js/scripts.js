981ea185970529652bdfce0ac5115bf993fef93b
Rick Thornbrugh <rthornbrug@gmail.com>
2016-06-17 16:18:57 -0700

    worked out the if lse statements backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,15 @@ $(document).ready(function(event) {
     var q2 = parseInt($("select#qts2").val());
     var q3 = parseInt($("select#qts3").val());
     var q4 = parseInt($("select#qts4").val());
+    var result = (q1 + q2 + q3 + q4);
+alert(result);
+  if (result <= 5) {
+    alert("Looks like Rome in the springtime!!");
+  } else if (result === 6) {
+    alert("Costa rica")
+  } else {
+    alert("Fiji")
+  }
 
   });
 });
