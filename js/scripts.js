7204332eb195a5fb6a8c42abbdb64d2c011ff6bf
Rick Thornbrugh <rthornbrug@gmail.com>
2016-06-17 17:44:21 -0700

    worked on redirect to tourism website based on responses

--- js/scripts.js
+++ js/scripts.js
@@ -20,20 +20,28 @@ $(document).ready(function(event) {
 });
 
 $(document).ready(function(event) {
-  $("form#questions").submit(function(event) {
+  $("form#questions").submit(function() {
     var q1 = parseInt($("select#qts1").val());
     var q2 = parseInt($("select#qts2").val());
     var q3 = parseInt($("select#qts3").val());
     var q4 = parseInt($("select#qts4").val());
+    var rome = ("http://www.aviewoncities.com/rome/romeattractions.html");
     var result = (q1 + q2 + q3 + q4);
-alert(result);
-  if (result <= 5) {
-    alert("Looks like Rome in the springtime!!");
-  } else if (result === 6) {
-    alert("Costa rica")
-  } else {
-    alert("Fiji")
-  }
+    alert(result);
+    alert(rome);
+    if (result <= 5) {
+      $(location).attr('href','http://www.aviewoncities.com/rome/romeattractions.html');
+
+    } else if (result === 6) {
+      $(location).attr('href','http://www.aviewoncities.com/rome/romeattractions.html');
+
+    } else {
+      $(location).attr('href','http://www.aviewoncities.com/rome/romeattractions.html');
+
+    }
+    event.preventDefault();
+
+
 
   });
 });
